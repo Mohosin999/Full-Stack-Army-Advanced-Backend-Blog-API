6896230556ee51ab3b51e4d5302d82c3f38406ed
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-09-01 07:56:09 +0600

    add user model

--- src/models/User.js
+++ src/models/User.js
@@ -1,17 +1,31 @@
-const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      maxLength: 50,
+      minLength: 5,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: String,
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "declined", "blocked"],
+      default: "pending",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-});
+  { timestamps: true, id: true }
+);
 
-const User = mongoose.model("User", userSchema);
-
+const User = model("User", userSchema);
 module.exports = User;
