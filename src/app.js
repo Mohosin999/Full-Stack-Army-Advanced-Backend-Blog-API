0b7566cb1c7ce4d7441f6b375b5391bf80c4f414
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-09-02 09:05:07 +0600

    refactor: app file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const connectDB = require("./db/db");
+const { connectDB } = require("./db");
 const applyMiddleware = require("./middleware");
 
 const app = express();
@@ -13,9 +13,28 @@ app.get("/health", (req, res) => {
   });
 });
 
-// connect to MongoDB
-connectDB().then(() => {
-  app.listen(4000, () => {
-    console.log("Server is listening on port 4000");
+app.use((err, _req, res, next) => {
+  // TODO: format error
+  console.log(err);
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
   });
 });
+
+// connect Database
+const main = async () => {
+  try {
+    await connectDB();
+    app.listen(4000, () => {
+      console.log("Server is listening on port 4000");
+    });
+  } catch (error) {
+    console.log("Database Error");
+    console.log(error);
+  }
+};
+
+main();
+
+module.exports = app;
